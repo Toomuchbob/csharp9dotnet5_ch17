b4bbfbd7d708edb1c6f2ca1ae5429b252364d4c4
Gino Zaccardelli <ginozacc@gmail.com>
2021-06-22 19:55:55 -0400

    storage of local files

---
+++ Models/ProductRegion.cs
@@ -0,0 +1,15 @@
+using Piranha.Extend;
+using Piranha.Extend.Fields;
+using Piranha.Extend.Models;
+
+namespace NorthwindCms.Models                                              
+{
+    public class ProductRegion
+    {
+        [Field(Title = "Product ID")]
+        public NumberField ProductID { get; set; }
+
+        [Field(Title = "Product name")]
+        
+    }
+}                                 
