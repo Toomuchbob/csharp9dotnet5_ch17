dc022e5bf7b97a5e1365ace24162d6ca59cf04df
Gino Zaccardelli <ginozacc@gmail.com>
2021-06-23 17:33:28 -0400

    Finish backup of local files

--- Models/ProductRegion.cs
+++ Models/ProductRegion.cs
@@ -10,6 +10,12 @@ namespace NorthwindCms.Models
         public NumberField ProductID { get; set; }
 
         [Field(Title = "Product name")]
-        
+        public TextField ProductName { get; set; }
+
+        [Field(Title = "Unit price", Options = FieldOption.HalfWidth)]
+        public StringField UnitPrice { get; set; }
+
+        [Field(Title = "Units in stock", Options = FieldOption.HalfWidth)]
+        public NumberField UnitsIStock { get; set; }
     }
 }                                 
